376108962a8fd52688ad4b6b65dd2f5acd53959c
mraghavi <ms.sairaghavi@gmail.com>
2024-08-07 00:11:26 +0530

    changed Sidenav layout and added links to projects

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -10,15 +10,37 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
-export class LayoutComponent {
+export class LayoutComponent  {
   title = 'Basics';
-  toggleSidebar(): void {
-    const sidebar = document.querySelector("#sidebar") as HTMLElement;
-    const container = document.querySelector(".my-container") as HTMLElement;
-    if (sidebar && container) {
-      sidebar.classList.toggle("active-nav");
-      container.classList.toggle("active-cont");
-    }
+  btnOpen!: HTMLElement;
+  btnClose!: HTMLElement;
+  fullNavItem!: HTMLElement;
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  ngAfterViewInit(): void {
+    this.btnOpen = document.querySelector('.btn-open') as HTMLElement;
+    this.btnClose = document.querySelector('.btn-close') as HTMLElement;
+    this.fullNavItem = document.querySelector('.full-nav-item') as HTMLElement;
+
+    this.btnOpen.addEventListener('click', this.openNav.bind(this));
+    this.btnClose.addEventListener('click', this.closeNav.bind(this));
+  }
+
+  openNav(): void {
+    this.fullNavItem.classList.add('show-full-nav-item');
+    document.querySelector('.wrapper-header')?.classList.add('swipe-wrapper-header');
+    this.btnClose.style.display = 'block';
+    this.btnOpen.style.display = 'none';
+  }
+
+  closeNav(): void {
+    this.fullNavItem.classList.remove('show-full-nav-item');
+    document.querySelector('.wrapper-header')?.classList.remove('swipe-wrapper-header');
+    this.btnClose.style.display = 'none';
+    this.btnOpen.style.display = 'block';
   }
   
 }
